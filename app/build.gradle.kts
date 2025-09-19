a8ce3c90d36555d0d714e0a05dc2e0d87c97d96a
Jonathan Blandon <jonathanblandon1017@gmail.com>
2025-09-19 16:16:41 -0500

    📱 Bump version to 1.3.10 (Code: 30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.libraryinventoryapp"
         minSdk = 26  // Android 8.0+ (Cambiado desde Android 7.0)
         targetSdk = 34  // Android 14 (última versión)
-        versionCode = 29
-        versionName = "1.3.9"
+        versionCode = 30
+        versionName = "1.3.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
